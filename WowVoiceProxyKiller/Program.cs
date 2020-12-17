2de12e696d37cb2fd4a0e020247cb08d0c574b01
David <David@c0degeek.dev>
2020-12-17 13:00:45 +0100

    Simplified implementation

--- WowVoiceProxyKiller/Program.cs
+++ WowVoiceProxyKiller/Program.cs
@@ -7,7 +7,9 @@ namespace WowVoiceProxyKiller
 {
     public class Program
     {
-        public static readonly string[] ProcessNames = { "WowVoiceProxy", "WowVoiceProxyT", "World of Warcraft Voice Proxy" };
+        public static readonly List<string> ProcessNames = new List<string>
+            {"WowVoiceProxy", "WowVoiceProxyT", "World of Warcraft Voice Proxy"};
+
         public static bool IsRunning = true;
         public static int SleepInterval = 10000;
         public static int TotalKilled;
@@ -20,7 +22,10 @@ namespace WowVoiceProxyKiller
 
             while (IsRunning)
             {
-                GetProcesses().ForEach(process =>
+                var foundProcesses = new List<Process>();
+                ProcessNames.ForEach(processName => foundProcesses.AddRange(Process.GetProcessesByName(processName)));
+
+                foundProcesses.ForEach(process =>
                 {
                     if (process.HasExited) return;
 
@@ -33,7 +38,7 @@ namespace WowVoiceProxyKiller
                         process.CloseMainWindow();
                         process.WaitForExit();
                     }
-                    
+
                     process.Dispose();
 
                     TotalKilled++;
@@ -42,18 +47,6 @@ namespace WowVoiceProxyKiller
 
                 Thread.Sleep(SleepInterval);
             }
-        }
-
-        private static List<Process> GetProcesses()
-        {
-            var foundProcesses = new List<Process>();
-            
-            foreach (var processName in ProcessNames)
-            {
-                foundProcesses.AddRange(Process.GetProcessesByName(processName));
-            }
-
-            return foundProcesses;
         }
 
         private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
